00eb15d5b58f25bea86bbe4ebe46df95e141fa98
truskk <107281769+truskk@users.noreply.github.com>
2024-06-27 04:20:45 -0400

    Update FilterEngine.cs

--- Core/FilterEngine.cs
+++ Core/FilterEngine.cs
@@ -390,17 +390,6 @@ namespace Coflnet.Sky.Filter
             return Expression.Lambda<Func<T, bool>>(body, p);
         }
 
-        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> a, Expression<Func<T, bool>> b)
-        {
-
-            ParameterExpression p = a.Parameters[0];
-
-            SubstExpressionVisitor visitor = new SubstExpressionVisitor();
-            visitor.subst[b.Parameters[0]] = p;
-
-            Expression body = Expression.OrElse(a.Body, visitor.Visit(b.Body));
-            return Expression.Lambda<Func<T, bool>>(body, p);
-        }
     }
 
     internal class SubstExpressionVisitor : System.Linq.Expressions.ExpressionVisitor
